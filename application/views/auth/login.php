00ab81b755c1374674901b4b76d2d57c09d7779e
HanifRifan <hanifrifan88@gmail.com>
2024-12-29 12:36:52 +0700

    update tampilan login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,26 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Admin</title>
+    <title>Manajemen Pelabuhan</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/login.css'); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="login-container">
-        <h2>Login Pelabuhan</h2>
-        <?php if (!empty($error)): ?>
-            <div class="error-message"><?php echo $error; ?></div>
-        <?php endif; ?>
-        <form method="post" action="login">
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username" placeholder="Masukkan username" required>
+        <div class="form-container">
+            <div class="mb-4">
+                <span class="fs-4 fw-bold">Login Pelabuhan</span>
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" placeholder="Masukkan password" required>
-            </div>
-            <button type="submit">Login</button>
-        </form>
+            <form action="login" method="post">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" placeholder="Masukkan username" required />
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Masukkan password" required />
+                </div>
+                <button class="btn btn-dark w-100">Login</button>
+            </form>
+        </div>
+        <div class="image-container">
+            <img src="<?php echo base_url('assets/images/image_login.png'); ?>" alt="Welcome Aboard">
+        </div>
     </div>
 </body>
 </html>
